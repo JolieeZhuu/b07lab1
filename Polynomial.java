11b5b4b8857437050636393a272a0c691bb92fd3
Jolie Zhu <zhujolie973@gmail.com>
2026-05-28 10:50:59 -0400

    save changes prior to testing

--- Polynomial.java
+++ Polynomial.java
@@ -1,13 +1,41 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Polynomial {
 	
 	private double[] coefficients;
+    private int[] exponents;
 	
     public Polynomial() {
     	coefficients = new double[1];
+        exponents = new int[1];
     }
     
-    public Polynomial(double[] coefficients) {
+    public Polynomial(double[] coefficients, int[] exponents) {
     	this.coefficients = coefficients;
+        this.exponents = exponents;
+    }
+
+    public Polynomial(File file) {
+        Scanner sc = new Scanner(file);
+        String line = sc.nextLine();
+        
+        String[] tmpArr = line.split("(?=[-+])");
+        int len = tmpArr.length;
+        coefficients = new double[len];
+        exponents = new int[len];
+
+        for (int i = 0; i < len; i++) {
+            String tmp = tmpArr[i];
+            int idx = tmp.indexOf("x");
+            if (idx == -1) {
+                exponents[i] = 0;
+            } else {
+                exponents[i] = Integer.parseInt(tmp.substring(idx + 1, tmp.length));
+            }
+            coefficients[i] = Double.parseDouble(tmp.substring(0, idx));
+        }
     }
     
     public double[] getCoefficients() {
@@ -16,6 +44,14 @@ public class Polynomial {
     
     public void setCoefficients(double[] coefficients) {
     	this.coefficients = coefficients;
+    }
+
+    public int[] getExponents() {
+        return exponents;
+    }
+
+    public void setExponents(int[] exponents) {
+        this.exponents = exponents;
     }
     
     public Polynomial add(Polynomial p) {
@@ -54,5 +90,56 @@ public class Polynomial {
     public boolean hasRoot(double x) {
     	double res = evaluate(x);
     	return res == 0;
+    }
+
+    public Polynomial multiply(Polynomial p) {
+        int maxL = p.getCoefficients().length * coefficients.length;
+        int tempExp[] = new int[maxL];
+
+        // find exponents
+        int idx = 0;
+        for (int i = 0; i < coefficients.length; i++) {
+            for (int j = 0; j < p.getCoefficients().length; j++) {
+                int tmp = Array.binarySearch(tempExp, exponents[i] * p.getExponents()[j]);
+                if (tmp != -1) {
+                    tempExp[idx] = exponents[i] * p.getExponents()[j];
+                    tmp++;
+                }
+            }
+        }
+        Arrays.sort(tempExp);
+        int newExp[] = new int[idx];
+        // copy exponents to correct array length
+        for (int i = 0; i < idx; i++) {
+            newExp[i] = tempExp[i];
+        }
+
+        // find coefficients
+        int newCoeffs[] = new int[idx];
+        for (int i = 0; i < coefficients.length; i++) {
+            for (int j = 0; j < p.getCoefficients().length; j++) {
+                int coeff = coefficients[i] * p.getCoefficients()[j];
+                int tmp = Array.binarySearch(tempExp, exponents[i] * p.getExponents()[j]);
+                newCoeffs[tmp] += coeff;
+            }
+        }
+        Polynomial newP = new Polynomial(newCoeffs, newExp);
+        return newP;
+    }
+
+    public void saveToFile(String fileName) {
+        String s = "";
+        int len = coefficients.length;
+        for (int i = 0; i < len; i++) {
+            if (exponents[i] == 0) {
+                s += "" + coefficients[i] + "";
+            } else {
+                if (coefficients[i] > 0) s += "+" + coefficients[i] + "x" + exponents[i];
+                else s += "" + coefficients[i] + "x" + exponents[i];
+            }
+        }
+        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(fileName)));
+        out.write(s);
+        out.close();
     }
 }
